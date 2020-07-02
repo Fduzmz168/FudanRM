7b1e137d0f1f5cd3b00c8fa75f785446b2bd8cea
4AKker <39761308+4AKker@users.noreply.github.com>
2020-07-02 19:08:26 +0800

    Part 1 completed
    主页面设计（导航栏等）；数据库功能的前端实现；图片上传

--- UI/src/router/index.js
+++ UI/src/router/index.js
@@ -1,40 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import AppIndex from "../components/home/AppIndex"
-import Login from "../components/Login";
+import Login from "../components/Login"
+import Home from "../components/Home"
+import LibraryIndex from "../components/library/LibraryIndex"
 
 Vue.use(VueRouter)
 
 const routes = [
-      {
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home,
+        // home页面并不需要被访问
+        redirect: '/index',
+        children: [
+            {
+                path: '/index',
+                name: 'AppIndex',
+                component: AppIndex,
+                // 区分页面是否需要拦截
+                meta: {
+                    requireAuth: true
+                }
+            },
+            {
+                path: '/library',
+                name: 'Library',
+                component: LibraryIndex,
+                meta: {
+                    requireAuth: true
+                }
+            },
+        ]
+    },
+
+    {
         path: '/login',
         name: 'Login',
         component: Login
-      },
-      {
-        path: '/index',
-        name: 'AppIndex',
-        component: AppIndex,
-        // 区分页面是否需要拦截
-        meta: {
-          requireAuth: true
-        }
-      },
+    },
 
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
